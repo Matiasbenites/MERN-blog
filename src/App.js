4cdc554c43da44178edeafec4a333cbe856e72c6
Matias Benites <matiasbenites665@gmail.com>
2023-02-23 19:36:21 -0300

    feat:preview of img's post

--- src/App.js
+++ src/App.js
@@ -12,6 +12,51 @@ function App() {
           <a href="/">Register</a>
         </nav>
       </header>
+      <div className="post">
+        <img
+          src="https://techcrunch.com/wp-content/uploads/2022/12/lawnmower-Large.jpeg?w=1390&crop=1"
+          alt=""
+        />
+        <h2>Full-house battery backup coming later this year</h2>
+        <p>
+          Today at its special launch event, home backup power giant EcoFlow
+          launched a flurry of new products, including a “Whole-Home Backup
+          Power Solution.” The company also launched a battery-powered fridge
+          and a portable battery-powered air-conditioning unit, which both makes
+          sense. There’s also a lawn mower, which makes me think that preppers
+          will have delightfully pristine lawns, even as the apocalypse looms.
+        </p>
+      </div>
+      <div className="post">
+        <img
+          src="https://techcrunch.com/wp-content/uploads/2022/12/lawnmower-Large.jpeg?w=1390&crop=1"
+          alt=""
+        />
+        <h2>Full-house battery backup coming later this year</h2>
+        <p>
+          Today at its special launch event, home backup power giant EcoFlow
+          launched a flurry of new products, including a “Whole-Home Backup
+          Power Solution.” The company also launched a battery-powered fridge
+          and a portable battery-powered air-conditioning unit, which both makes
+          sense. There’s also a lawn mower, which makes me think that preppers
+          will have delightfully pristine lawns, even as the apocalypse looms.
+        </p>
+      </div>
+      <div className="post">
+        <img
+          src="https://techcrunch.com/wp-content/uploads/2022/12/lawnmower-Large.jpeg?w=1390&crop=1"
+          alt=""
+        />
+        <h2>Full-house battery backup coming later this year</h2>
+        <p>
+          Today at its special launch event, home backup power giant EcoFlow
+          launched a flurry of new products, including a “Whole-Home Backup
+          Power Solution.” The company also launched a battery-powered fridge
+          and a portable battery-powered air-conditioning unit, which both makes
+          sense. There’s also a lawn mower, which makes me think that preppers
+          will have delightfully pristine lawns, even as the apocalypse looms.
+        </p>
+      </div>
     </main>
   );
 }
