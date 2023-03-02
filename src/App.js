3c6a612a1618ecfc41ef189f2f0ba8a984fb6aef
Matias Benites <matiasbenites665@gmail.com>
2023-03-02 12:11:35 -0300

    feat:Added page's links

--- src/App.js
+++ src/App.js
@@ -1,90 +1,17 @@
+import React from "react";
+import Post from "./components/Post";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
+import Layout from "./components/Layout";
 
 function App() {
   return (
-    <main>
-      <header>
-        <a href="/" className="logo">
-          MyBloggy
-        </a>
-        <nav>
-          <a href="/">Login</a>
-          <a href="/">Register</a>
-        </nav>
-      </header>
-      <div className="post">
-        <div className="image">
-          <img
-            src="https://techcrunch.com/wp-content/uploads/2022/12/lawnmower-Large.jpeg?w=1390&crop=1"
-            alt=""
-          />
-        </div>
-        <div className="texts">
-          <h2>Full-house battery backup coming later this year</h2>
-          <p className="info">
-            <span className="author">NAMENAME</span>
-            <time>26-02-2023 11:10</time>
-          </p>
-          <p>
-            Today at its special launch event, home backup power giant EcoFlow
-            launched a flurry of new products, including a “Whole-Home Backup
-            Power Solution.” The company also launched a battery-powered fridge
-            and a portable battery-powered air-conditioning unit, which both
-            makes sense. There’s also a lawn mower, which makes me think that
-            preppers will have delightfully pristine lawns, even as the
-            apocalypse looms.
-          </p>
-        </div>
-      </div>
-      <div className="post">
-        <div className="image">
-          <img
-            src="https://techcrunch.com/wp-content/uploads/2022/12/lawnmower-Large.jpeg?w=1390&crop=1"
-            alt=""
-          />
-        </div>
-        <div className="texts">
-          <h2>Full-house battery backup coming later this year</h2>
-          <p className="info">
-            <span className="author">NAMENAME</span>
-            <time>26-02-2023 11:10</time>
-          </p>
-          <p>
-            Today at its special launch event, home backup power giant EcoFlow
-            launched a flurry of new products, including a “Whole-Home Backup
-            Power Solution.” The company also launched a battery-powered fridge
-            and a portable battery-powered air-conditioning unit, which both
-            makes sense. There’s also a lawn mower, which makes me think that
-            preppers will have delightfully pristine lawns, even as the
-            apocalypse looms.
-          </p>
-        </div>
-      </div>
-      <div className="post">
-        <div className="image">
-          <img
-            src="https://techcrunch.com/wp-content/uploads/2022/12/lawnmower-Large.jpeg?w=1390&crop=1"
-            alt=""
-          />
-        </div>
-        <div className="texts">
-          <h2>Full-house battery backup coming later this year</h2>
-          <p className="info">
-            <span className="author">NAMENAME</span>
-            <time>26-02-2023 11:10</time>
-          </p>
-          <p>
-            Today at its special launch event, home backup power giant EcoFlow
-            launched a flurry of new products, including a “Whole-Home Backup
-            Power Solution.” The company also launched a battery-powered fridge
-            and a portable battery-powered air-conditioning unit, which both
-            makes sense. There’s also a lawn mower, which makes me think that
-            preppers will have delightfully pristine lawns, even as the
-            apocalypse looms.
-          </p>
-        </div>
-      </div>
-    </main>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Post />} />
+        <Route path={"/login"} element={<div>Login</div>} />
+      </Route>
+    </Routes>
   );
 }
 
