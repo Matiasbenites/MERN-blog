9e6c4e4c2bd5deebe74a556a9b707ba10b61ded1
Matias Benites <matiasbenites665@gmail.com>
2023-03-04 20:29:54 -0300

    feat:LoginPage created

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Layout from "./Components/Layout";
 import IndexPage from "./Pages/IndexPage";
+import LoginPage from "./Pages/LoginPage";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<IndexPage />} />
-        <Route path={"/login"} element={<div>Login</div>} />
+        <Route path={"/login"} element={<LoginPage />} />
       </Route>
     </Routes>
   );
