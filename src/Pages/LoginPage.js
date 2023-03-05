be2be91c9d4c0bf2c2da784d4c722ec5dbefa7ed
Matias Benites <matiasbenites665@gmail.com>
2023-03-05 17:25:01 -0300

    feat:Loginpage created;form added

--- src/Pages/LoginPage.js
+++ src/Pages/LoginPage.js
@@ -1,5 +1,12 @@
 export default function LoginPage() {
-  return <div>
-    Login Page
-  </div>;
+  return (
+    <div>
+      <form className="login">
+        <h1>Login</h1>
+        <input type="text" placeholder="username"></input>
+        <input type="password" placeholder="password"></input>
+        <button>Login</button>
+      </form>
+    </div>
+  );
 }
