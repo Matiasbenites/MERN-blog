3ef11bd957b8c2710599a45f6b8212d95ccbe130
Matias Benites <matiasbenites665@gmail.com>
2023-03-05 17:21:07 -0300

    feat:Register's route added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 import Layout from "./Components/Layout";
 import IndexPage from "./Pages/IndexPage";
 import LoginPage from "./Pages/LoginPage";
+import RegisterPage from "./Pages/RegisterPage";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Route path="/" element={<Layout />}>
         <Route index element={<IndexPage />} />
         <Route path={"/login"} element={<LoginPage />} />
+        <Route path={"/register"} element={<RegisterPage />} />
       </Route>
     </Routes>
   );
