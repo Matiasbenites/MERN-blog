07e1fe1dad2cf3ad314533960b8ccb846fdd6f7e
Matias Benites <matiasbenites665@gmail.com>
2023-03-07 20:40:32 -0300

    feat:index's routes created;not finished yet

---
+++ backend/routes/indexRoute.js
@@ -0,0 +1,10 @@
+// import express from "express";
+// import registerRoute from "./registerRoute";
+
+// const indexRoute = express.Router();
+
+// indexRoute.use("/register", registerRoute);
+// // indexRoute.get("/api", loginRoute);
+// // indexRoute.get("/api", homeRoute);
+
+// export default indexRoute;
