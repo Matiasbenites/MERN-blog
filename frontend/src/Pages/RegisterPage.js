bb4ec37cecad0d1179c42e7633cf9a2c82bb1ee2
Matias Benites <matiasbenites665@gmail.com>
2023-03-07 20:38:35 -0300

    feat:added fetch to api

--- frontend/src/Pages/RegisterPage.js
+++ frontend/src/Pages/RegisterPage.js
@@ -1,13 +1,39 @@
+import { useState } from "react";
 export default function RegisterPage() {
+  const [username, setUsername] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+  async function register(ev) {
+    ev.preventDefault();
+    fetch("http://localhost:4000/register", {
+      method: "POST",
+      body: JSON.stringify({ username, email, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+  }
   return (
-    <div>
-      <form className="register">
-        <h1>Register</h1>
-        <input type="text" placeholder="username"></input>
-        <input type="email" placeholder="email"></input>
-        <input type="password" placeholder="password"></input>
-        <button>Register</button>
-      </form>
-    </div>
+    <form className="register" onSubmit={register}>
+      <h1>Register</h1>
+      <input
+        type="text"
+        placeholder="username"
+        value={username}
+        onChange={(ev) => setUsername(ev.target.value)}
+      ></input>
+      <input
+        type="email"
+        placeholder="email"
+        value={email}
+        onChange={(ev) => setEmail(ev.target.value)}
+      ></input>
+      <input
+        type="password"
+        placeholder="password"
+        value={password}
+        onChange={(ev) => setPassword(ev.target.value)}
+      ></input>
+      <button>Register</button>
+    </form>
   );
 }
