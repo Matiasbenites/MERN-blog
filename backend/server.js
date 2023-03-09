6cf7c520669b35f150172fc829ca0642ed700cae
Matias Benites <matiasbenites665@gmail.com>
2023-03-09 19:40:06 -0300

    feat:Added mongo's connection;added indexRoute

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,27 @@
-import express from "express";
-import cors from "cors";
-import registerRoute from "./routes/registerRoute.js";
+const express = require("Express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const indexRoute = require("./routes/indexRoute.js");
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-app.use("/register", registerRoute);
-app.get("/", (req, res) => {
-  res.send("Hello World! 💫💫");
-});
+app.use("/", indexRoute);
+
+mongoose
+  .connect(
+    "mongodb+srv://blog:v440kAezADLLThC1@cluster0.d3hin0y.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(() => console.log("Conected to MongoDB Atlas"))
+  .catch((error) => console.error(error));
 
 app.listen(4000, "localhost", () => {
   console.log("Servidor funcionando correctamente en puerto 4000  💫💫💫");
 });
+
+//blog
+//v440kAezADLLThC1
+//mongodb+srv://blog:v440kAezADLLThC1@cluster0.d3hin0y.mongodb.net/?retryWrites=true&w=majority
+//
