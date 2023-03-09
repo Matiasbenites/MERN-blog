41cd54b1b63b6d146b766823e2fe6983a474f39a
Matias Benites <matiasbenites665@gmail.com>
2023-03-09 17:24:27 -0300

    feat:Added registration's alert

--- frontend/src/Pages/RegisterPage.js
+++ frontend/src/Pages/RegisterPage.js
@@ -1,16 +1,22 @@
 import { useState } from "react";
+
 export default function RegisterPage() {
-  const [username, setUsername] = useState();
-  const [email, setEmail] = useState();
-  const [password, setPassword] = useState();
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   async function register(ev) {
     ev.preventDefault();
-    fetch("http://localhost:4000/register", {
+    const response = await fetch("http://localhost:4000/register", {
       method: "POST",
       body: JSON.stringify({ username, email, password }),
       headers: { "Content-Type": "application/json" },
     });
+    if (response.status === 200) {
+      alert("registration successful");
+    } else {
+      alert("registration failed");
+    }
   }
   return (
     <form className="register" onSubmit={register}>
