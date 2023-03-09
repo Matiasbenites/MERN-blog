fd3a580bb733772a602292ad768246379db6de30
Matias Benites <matiasbenites665@gmail.com>
2023-03-09 19:32:31 -0300

    feat:Added post's endpoint

--- backend/routes/registerRoute.js
+++ backend/routes/registerRoute.js
@@ -1,10 +1,22 @@
-import express from "express";
+const express = require("Express");
+const userModel = require("../models/userModel");
 
 const registerRoute = express.Router();
 
-registerRoute.post("/register", (req, res) => {
-  const { username, password } = req.body;
-  res.json({ requestData: { username, password } });
+registerRoute.post("/", async (req, res) => {
+  const { username, email, password } = req.body;
+  try {
+    const userDoc = await userModel.create({
+      username,
+      email,
+      password,
+      // bcrypt.hashSync(password, salt),
+    });
+    res.json(userDoc);
+  } catch (e) {
+    console.log(e);
+    res.status(400).json(e);
+  }
 });
 
-export default registerRoute;
+module.exports = registerRoute;
