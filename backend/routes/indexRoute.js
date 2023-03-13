6ec5524a9163e55498d2094cb4887fb7e33035d6
Matias Benites <matiasbenites665@gmail.com>
2023-03-13 09:45:18 -0300

    feat:Added loginRoute to index

--- backend/routes/indexRoute.js
+++ backend/routes/indexRoute.js
@@ -1,10 +1,11 @@
 const express = require("Express");
+const loginRoute = require("./loginRoute");
 const registerRoute = require("./registerRoute");
 
 const indexRoute = express.Router();
 
 indexRoute.use("/register", registerRoute);
-// indexRoute.get("/login", loginRoute);
+indexRoute.get("/login", loginRoute);
 // indexRoute.get("/home", homeRoute);
 
 module.exports = indexRoute;
